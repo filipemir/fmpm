d11282ae9edec1893dfc3b529c189f8ffff6cda0
fmiranda <filipe.m.mir@gmail.com>
2020-03-31 18:47:30 -0400

    makes nav links trail in

--- src/components/NavHorizontal/index.tsx
+++ src/components/NavHorizontal/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { ReactSVG } from 'react-svg';
 import useMeasure, { RectReadOnly } from 'react-use-measure';
-import { useSpring, animated } from 'react-spring';
+import { useSpring, animated, useTrail } from 'react-spring';
 
 import { RootDiv, PageDiv, PageSeparator, underlineDivCss, dotCss } from './styles';
 import underlineSvg from 'images/underline.svg';
@@ -32,7 +32,8 @@ export default function NavHorizontal({
         [skipAnimations, setSkipAnimations] = useState(true),
         [activePageRef, activePageRect] = useMeasure(),
         underlinePosition = useSpring({ ...getUnderlinePosition(activePageRect), immediate: skipAnimations }),
-        dotPosition = useSpring({ ...getDotPosition(activePageRect), immediate: skipAnimations });
+        dotPosition = useSpring({ ...getDotPosition(activePageRect), immediate: skipAnimations }),
+        trail = useTrail(pages.length, { opacity: 1, from: { opacity: 0 } });
 
     return (
         <RootDiv>
@@ -40,11 +41,12 @@ export default function NavHorizontal({
             <animated.div style={underlinePosition} css={underlineDivCss}>
                 <ReactSVG src={underlineSvg} />
             </animated.div>
-            {pages.map((p, i) => {
-                const isActive = p === currentPage,
+            {trail.map(({ opacity }, i) => {
+                const p = pages[i],
+                    isActive = p === currentPage,
                     isLast = i === pages.length - 1;
                 return (
-                    <span key={`page-${p}`}>
+                    <animated.span style={{ opacity }} key={`page-${p}`}>
                         <PageDiv
                             active={isActive}
                             ref={isActive ? activePageRef : undefined}
@@ -56,7 +58,7 @@ export default function NavHorizontal({
                             {p}
                         </PageDiv>
                         {!isLast && <PageSeparator>.</PageSeparator>}
-                    </span>
+                    </animated.span>
                 );
             })}
         </RootDiv>
