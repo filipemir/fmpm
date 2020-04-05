d8a91ee95434302281b5b8efc169a7116a010e8e
fmiranda <filipe.m.mir@gmail.com>
2020-04-05 11:32:15 -0400

    adds school names to degree cards

--- src/components/DegreeCard/index.tsx
+++ src/components/DegreeCard/index.tsx
@@ -3,19 +3,22 @@ import format from 'date-fns/format';
 import getYear from 'date-fns/getYear';
 import parse from 'react-html-parser';
 
-import { RootDiv, TopRowDiv, DegreeNameDiv, DatesDiv, DescriptionDiv } from './styles';
+import { RootDiv, TopRowDiv, DegreeNameDiv, DatesDiv, DescriptionDiv, SchoolSpan } from './styles';
 import { Degree } from 'models/experience';
 
 const DATE_FORMAT = 'MMMM yyyy';
 
 export default function DegreeCard({ degree }: { degree: Degree }) {
-    const { startDate, endDate, description, name } = degree,
+    const { startDate, endDate, description, school, name } = degree,
         sameYear = getYear(startDate) === getYear(endDate),
         dateStr = `${format(startDate, sameYear ? 'MMMM' : DATE_FORMAT)} to ${format(endDate, DATE_FORMAT)}`;
     return (
         <RootDiv>
             <TopRowDiv>
-                <DegreeNameDiv>{name}</DegreeNameDiv>
+                <DegreeNameDiv>
+                    {name}
+                    <SchoolSpan>{school}</SchoolSpan>
+                </DegreeNameDiv>
             </TopRowDiv>
             <DatesDiv>{dateStr}</DatesDiv>
             {description && <DescriptionDiv>{parse(description)}</DescriptionDiv>}
--- src/components/DegreeCard/styles.ts
+++ src/components/DegreeCard/styles.ts
@@ -1,6 +1,21 @@
+import styled from '@emotion/styled';
+
 import { ExperienceRootDiv, DescriptionDiv, TitleSpan } from 'styles/experience-cards';
+import { COLOR_ACCENT, COLOR_TERNARY } from 'styles/global';
 
 export const RootDiv = ExperienceRootDiv;
 export const DegreeNameDiv = TitleSpan;
+export const SchoolSpan = styled.span`
+    color: ${COLOR_ACCENT};
+    text-shadow: none;
+    font-style: normal;
+    font-size: 0.8em;
+
+    &:before {
+        content: ' | ';
+        color: ${COLOR_TERNARY};
+        font-weight: 100;
+    }
+`;
 
 export { TopRowDiv, TechDiv, DatesDiv, DescriptionDiv } from 'styles/experience-cards';
