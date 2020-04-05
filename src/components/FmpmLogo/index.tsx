af493268170de27f2dec9bd257f08000081a4206
fmiranda <filipe.m.mir@gmail.com>
2020-04-05 11:58:03 -0400

    animates logo slightly

--- src/components/FmpmLogo/index.tsx
+++ src/components/FmpmLogo/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useSpring, animated, config } from 'react-spring';
 
 import { RootAnchor } from './styles';
 
 export default function FmpmLogo() {
-    return <RootAnchor>fmpm</RootAnchor>;
+    const spring = useSpring({ opacity: 1, from: { opacity: 0 }, config: config.molasses });
+    return (
+        <RootAnchor>
+            <animated.div style={spring}>fmpm</animated.div>
+        </RootAnchor>
+    );
 }
