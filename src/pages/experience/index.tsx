c06d47e3245cb9b4e8acfc7a795b05fbb592b9fb
fmiranda <filipe.m.mir@gmail.com>
2020-04-20 23:39:01 -0400

    adds resume PDF

--- src/pages/experience/index.tsx
+++ src/pages/experience/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import Resume from 'components/Resume';
-import { ResumeWrapper, RootDiv } from './styles';
+import { ResumeWrapper, RootDiv, ResumeButton, Note } from './styles';
 import { SectionHeaderWrapper, SectionIntro, Section } from 'styles/sections';
 import Header from 'components/Header';
 import Helmet from 'components/Helmet';
 import Page from 'models/page';
-
+import PdfSvg from './pdf.svg';
 const DESCRIPTION =
     "My online resume. I've been lucky to do a lot of interesting things over the years. This is where I keep a running tally.";
 
@@ -21,7 +21,14 @@ export default function HomePage() {
                 <SectionIntro>
                     I&apos;ve been lucky to wear a lot of different hats on my way to software engineering: teacher,
                     researcher, reporter, analyst, consultant. Not all those roles connect directly to the work I do
-                    today, but each taught me something that has made me a better engineer. So here is the full list:
+                    today, but each taught me something that has made me a better engineer. The full list is below.
+                    <Note>
+                        For a much abbreviated print version, see my{' '}
+                        <ResumeButton href={`/resume.pdf`} target={'_blank'} rel='noopener noreferrer'>
+                            resume
+                        </ResumeButton>
+                        .
+                    </Note>
                 </SectionIntro>
                 <ResumeWrapper>
                     <Resume />
