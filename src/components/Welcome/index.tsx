790f87c5581799a8dd468fa2e0640f7a134994d1
Filipe Miranda <filipe.miranda@buoyhealth.com>
2020-12-30 15:17:52 -0500

    update top technologies

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -24,7 +24,7 @@ import useMedia from 'use-media';
 import { PERSONAL_EMAIL } from 'data/personal';
 
 export default function Welcome() {
-    const tech = [Tech.REACT, Tech.TS, Tech.JAVA, Tech.SPRING_BOOT],
+    const tech = [Tech.REACT, Tech.TS, Tech.JAVA, Tech.PYTHON],
         spring = useSpring({ opacity: 1, from: { opacity: 0 } }),
         isMobile = useMedia({ maxWidth: '900px' });
     return (
