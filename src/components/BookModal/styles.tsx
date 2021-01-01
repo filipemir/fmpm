b56ed0d4123cc7ecd0c4c527338f30f109adf4d9
Filipe Miranda <filipe.miranda@buoyhealth.com>
2021-01-01 17:22:17 -0500

    Style modal for mobile

--- src/components/BookModal/styles.tsx
+++ src/components/BookModal/styles.tsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled';
 import { COLOR_SECONDARY, COLOR_TERNARY, FONT_FAMILY_SANS_SERIF, FONT_FAMILY_SERIF } from 'styles/global';
 
+const MOBILE_THRESHOLD = '900px';
+
 export const StyledRoot = styled.div`
     position: fixed;
     z-index: 100;
@@ -24,17 +26,35 @@ export const StyledContent = styled.div`
     left: 50%;
     transform: translate(-50%, -50%);
     cursor: pointer;
+
+    @media (max-width: ${MOBILE_THRESHOLD}) {
+        height: auto;
+    }
 `;
 
 export const StyledCover = styled.img`
     height: 100%;
-    box-shadow: rgba(2, 12, 27, 0.5) 10px 20px 30px 0px;
+    box-shadow: rgba(2, 12, 27, 0.5) 10px 20px 30px 0;
+
+    @media (max-width: ${MOBILE_THRESHOLD}) {
+        height: auto;
+        max-height: 50vh;
+        object-fit: cover;
+        object-position: center center;
+    }
 `;
 
 export const StyledInfo = styled.div`
     position: relative;
     top: -40%;
     transform: translate(50%, -50%);
+
+    @media (max-width: ${MOBILE_THRESHOLD}) {
+        max-width: 100%;
+        left: 30px;
+        top: -40px;
+        transform: unset;
+    }
 `;
 
 export const StyledTitle = styled.a`
