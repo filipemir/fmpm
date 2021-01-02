691bf9ede9ac4b9c24cb3fcff42449c6a04dcb4d
Filipe Miranda <filipe.miranda@buoyhealth.com>
2021-01-01 20:22:22 -0500

    Fix linting error

--- src/components/BookModal/stories.tsx
+++ src/components/BookModal/stories.tsx
@@ -9,6 +9,7 @@ const bookTitles = BOOKS_SORTED.map((b) => b.title);
 
 storiesOf(`BookModal`, module).add(`default`, () => {
     const bookTitle = select('Book', bookTitles, bookTitles[0]),
-        book = BOOKS_SORTED.find((b) => b.title === bookTitle)!;
+        book =
+            BOOKS_SORTED.find((b) => b.title === bookTitle) || BOOKS_SORTED[0];
     return <BookModal book={book} onClose={action('Close modal')} />;
 });
