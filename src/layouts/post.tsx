1610bd323e6709373896956e9c36551062407552
Filipe Miranda <filipe.miranda@buoyhealth.com>
2021-02-16 14:28:58 -0500

    Style blog some more

--- src/layouts/post.tsx
+++ src/layouts/post.tsx
@@ -1,16 +1,49 @@
 import React, { ReactNode } from 'react';
 import styled from '@emotion/styled';
-import { PADDING_TOP_PAGE } from 'styles/global';
+import {
+    COLOR_ACCENT,
+    COLOR_BG_ACCENT,
+    COLOR_PRIMARY,
+    FONT_FAMILY_SERIF,
+    PADDING_TOP_PAGE
+} from 'styles/global';
 
 export const Root = styled.div`
     margin: 0 auto;
     padding-top: ${PADDING_TOP_PAGE}px;
     padding-bottom: ${PADDING_TOP_PAGE}px;
     min-height: 100vh;
-    max-width: 90ch;
+    max-width: 75ch;
 
     h1 {
-        color: red;
+        font-family: ${FONT_FAMILY_SERIF};
+        font-size: 55px;
+        color: ${COLOR_ACCENT};
+        font-weight: 400;
+        z-index: 2;
+        text-shadow: 2px 4px 0 ${COLOR_BG_ACCENT};
+
+        font-style: italic;
+        margin: 50px 0 40px 0;
+
+        letter-spacing: -0.03em;
+        line-height: 1;
+    }
+
+    p,
+    li,
+    ol {
+        font-family: ${FONT_FAMILY_SERIF};
+        font-size: 20px;
+        line-height: 1.4;
+        font-weight: 300;
+        color: ${COLOR_PRIMARY};
+
+        letter-spacing: -0.03em;
+    }
+
+    strong {
+        font-weight: 600;
     }
 `;
 
