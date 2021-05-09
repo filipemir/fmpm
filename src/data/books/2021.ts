835f8ed9bf6e7fd64bab352c0a154ca1fd5a06b9
Filipe Miranda <filipe.miranda@buoyhealth.com>
2021-05-09 19:53:26 -0400

    Add two most recent books

--- src/data/books/2021.ts
+++ src/data/books/2021.ts
@@ -2,8 +2,28 @@ import parseISO from 'date-fns/parseISO';
 import { BookEntry } from 'models/media';
 import A_PROMISED_LAND_COVER from 'images/books/a-promised-land.jpg';
 import WHY_I_WRITE_COVER from 'images/books/why-i-write.jpg';
+import PARABLE_OF_THE_SOWER from 'images/books/parable-of-the-sower.jpeg';
+import TRUE_GRIT from 'images/books/true-grit.jpeg';
 
 export const BOOKS_2021: BookEntry[] = [
+    {
+        title: 'Parable of the Sower',
+        author: 'Octavia E. Butler',
+        url: 'https://www.indiebound.org/book/9781538732182',
+        startDate: parseISO('2021-04-21'),
+        endDate: parseISO('2021-05-07'),
+        favorite: false,
+        cover: PARABLE_OF_THE_SOWER
+    },
+    {
+        title: 'True Grit',
+        author: 'Charles Portis',
+        url: 'https://www.indiebound.org/book/9781590204597',
+        startDate: parseISO('2021-03-23'),
+        endDate: parseISO('2021-04-09'),
+        favorite: false,
+        cover: TRUE_GRIT
+    },
     {
         title: 'A Promised Land',
         author: 'Barack Obama',
